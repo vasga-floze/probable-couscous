88d4a6d8ff001d6123f51e1bce651945dbc860ca
@vasga-floze <gabriela.floze@gmail.com>
2021-06-25 17:23:19 -0600

    select and fetchall/fetchone | databases with python

--- intro-python/db.py
+++ intro-python/db.py
@@ -4,17 +4,22 @@ import mysql.connector #libreria que permite la conexion con mysql
 midb = mysql.connector.connect(
     #recibe cuatro argumentos
     host='localhost',
-    user='user_name',
-    password='password', 
-    database='name_database' #indicar el nombre de la bd
+    user='root',
+    password='root', 
+    database='pruebauno' #indicar el nombre de la bd
 ) #este metodo devuelve la instancia
 
 #con el metodo anterior obtenemos el cursor
 #un cursor es un objeto que permite interactuar con la db mediante lenguaje SQL
 cursor = midb.cursor()
 
+#esta consulta indica que se va a mostrar todos los registros en el modelo Usuario
 cursor.execute('select * from usuario')
 
+#fetchall() devuelve todas las instancias que encuentra
 resultado = cursor.fetchall()
+
+#fetchone() devuelve la primera que encuentra
+#resultado = cursor.fetchone()
 
 print(resultado)
