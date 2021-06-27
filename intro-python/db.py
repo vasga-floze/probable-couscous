ba5c4eca0ed3a52252504196827d7378284ac78a
@vasga-floze <gabriela.floze@gmail.com>
2021-06-27 00:37:13 -0600

    delete records | databases with python

--- intro-python/db.py
+++ intro-python/db.py
@@ -20,23 +20,33 @@ cursor = midb.cursor()
 # resultado = cursor.fetchall() #fetchall() DEVUELVE TODAS LAS INSTANCIAS QUE ENCUENTRA
 # print(resultado)
 
-#resultado = cursor.fetchone() #fetchone() DEVUELVE EL PRIMER REGISTRO QUE ENCUENTRA
+#resultado = cursor.fetchone() #fetchone() #DEVUELVE EL PRIMER REGISTRO QUE ENCUENTRA
+
+#CONSULTAR DEFINICIONES DE TABLAS (script con el que se creo la tabla)
+#cursor.execute('show create table usuario')
 
 
 #INSERTAR DATOS
-#cursor.execute('show create table usuario') #consultar script con el que se creo la tabla
 # sql = 'INSERT INTO usuario(nombre, apellido, edad) VALUES (%s, %s, %s)' #insertar datos
 # values = ('Carmen', 'Morataya', 56)
 # cursor.execute(sql, values)
-
 
 # midb.commit() #se compromenten los datos para que puedan ser guardados en la base de datos
 
 # print(cursor.rowcount)
 
-# #ACTUALIZAR DATOS
-sql = 'UPDATE usuario SET nombre = %s WHERE id = %s'
-values = ('Victoria', 6)
-cursor.execute(sql, values)
 
-midb.commit() #compromete datos para que se ejecute la consulte
+# #ACTUALIZAR DATOS
+# sql = 'UPDATE usuario SET nombre = %s WHERE id = %s'
+# values = ('Victoria', 6)
+# cursor.execute(sql, values)
+
+# midb.commit() #compromete datos para que se ejecute la consulta
+
+
+#ELIMINAR REGISTROS
+sql = 'DELETE FROM usuario WHERE id = %s'
+values = (9,) #agregar una coma despues dentro de la tupla para que la consulta pueda ser ejecutada
+cursor.execute(sql, values)
+midb.commit()
+print(cursor.rowcount)
