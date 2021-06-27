42ff0d239f76c30f4f8b7fb4c60b725064ad0723
@vasga-floze <gabriela.floze@gmail.com>
2021-06-27 00:57:15 -0600

    limiting records | databases with python

--- intro-python/db.py
+++ intro-python/db.py
@@ -13,25 +13,45 @@ midb = mysql.connector.connect(
 #un cursor es un objeto que permite interactuar con la db mediante lenguaje SQL
 cursor = midb.cursor()
 
+#LISTAR DATOS
 #esta consulta indica que se va a mostrar todos los registros en el modelo Usuario
-#cursor.execute('select * from usuario')
+# cursor.execute('select * from usuario')
+# resultado = cursor.fetchall() #fetchall() DEVUELVE TODAS LAS INSTANCIAS QUE ENCUENTRA
+# print(resultado)
+
+#LIMITANDO LOS RESULTADOS A MOSTRAR
+cursor.execute('select * from usuario limit 2')
+resultado = cursor.fetchall() #fetchall() DEVUELVE TODAS LAS INSTANCIAS QUE ENCUENTRA
+print(resultado)
+
+#DEVUELVE EL PRIMER REGISTRO QUE ENCUENTRA
+#resultado = cursor.fetchone() #fetchone() 
+
+#CONSULTAR DEFINICIONES DE TABLAS (script con el que se creo la tabla)
+#cursor.execute('show create table usuario')
 
 
 #INSERTAR DATOS
-#cursor.execute('show create table usuario') #consultar script con el que se creo la tabla
-sql = 'INSERT INTO usuario(nombre, apellido, edad) VALUES (%s, %s, %s)' #insertar datos
-values = ('Carmen', 'Morataya', 56)
-cursor.execute(sql, values)
+# sql = 'INSERT INTO usuario(nombre, apellido, edad) VALUES (%s, %s, %s)' #insertar datos
+# values = ('Carmen', 'Morataya', 56)
+# cursor.execute(sql, values)
+
+# midb.commit() #se compromenten los datos para que puedan ser guardados en la base de datos
+
+# print(cursor.rowcount)
 
 
-midb.commit() #se compromenten los datos para que puedan ser guardados en la base de datos
+# #ACTUALIZAR DATOS
+# sql = 'UPDATE usuario SET nombre = %s WHERE id = %s'
+# values = ('Victoria', 6)
+# cursor.execute(sql, values)
 
-print(cursor.rowcount)
+# midb.commit() #compromete datos para que se ejecute la consulta
 
-#fetchall() DEVUELVE TODAS LAS INSTANCIAS QUE ENCUENTRA
-#resultado = cursor.fetchall()
 
-#fetchone() DEVUELVE EL PRIMER REGISTRO QUE ENCUENTRA
-#resultado = cursor.fetchone()
-
-#print(resultado)
+#ELIMINAR REGISTROS
+# sql = 'DELETE FROM usuario WHERE id = %s'
+# values = (9,) #agregar una coma despues dentro de la tupla para que la consulta pueda ser ejecutada
+# cursor.execute(sql, values)
+# midb.commit()
+# print(cursor.rowcount)
