a7744fb160ddf3b3ef36489717b446fbaffc22a1
@vasga-floze <gabriela.floze@gmail.com>
2021-06-27 00:27:24 -0600

    update a record | databases with python

--- intro-python/db.py
+++ intro-python/db.py
@@ -5,7 +5,7 @@ midb = mysql.connector.connect(
     #recibe cuatro argumentos
     host='localhost',
     user='root',
-    password='root', 
+    password='root21', 
     database='pruebauno' #indicar el nombre de la bd
 ) #este metodo devuelve la instancia
 
@@ -13,25 +13,30 @@ midb = mysql.connector.connect(
 #un cursor es un objeto que permite interactuar con la db mediante lenguaje SQL
 cursor = midb.cursor()
 
+
+#LISTAR DATOS
 #esta consulta indica que se va a mostrar todos los registros en el modelo Usuario
-#cursor.execute('select * from usuario')
+# cursor.execute('select * from usuario')
+# resultado = cursor.fetchall() #fetchall() DEVUELVE TODAS LAS INSTANCIAS QUE ENCUENTRA
+# print(resultado)
+
+#resultado = cursor.fetchone() #fetchone() DEVUELVE EL PRIMER REGISTRO QUE ENCUENTRA
 
 
 #INSERTAR DATOS
 #cursor.execute('show create table usuario') #consultar script con el que se creo la tabla
-sql = 'INSERT INTO usuario(nombre, apellido, edad) VALUES (%s, %s, %s)' #insertar datos
-values = ('Carmen', 'Morataya', 56)
+# sql = 'INSERT INTO usuario(nombre, apellido, edad) VALUES (%s, %s, %s)' #insertar datos
+# values = ('Carmen', 'Morataya', 56)
+# cursor.execute(sql, values)
+
+
+# midb.commit() #se compromenten los datos para que puedan ser guardados en la base de datos
+
+# print(cursor.rowcount)
+
+# #ACTUALIZAR DATOS
+sql = 'UPDATE usuario SET nombre = %s WHERE id = %s'
+values = ('Victoria', 6)
 cursor.execute(sql, values)
 
-
-midb.commit() #se compromenten los datos para que puedan ser guardados en la base de datos
-
-print(cursor.rowcount)
-
-#fetchall() DEVUELVE TODAS LAS INSTANCIAS QUE ENCUENTRA
-#resultado = cursor.fetchall()
-
-#fetchone() DEVUELVE EL PRIMER REGISTRO QUE ENCUENTRA
-#resultado = cursor.fetchone()
-
-#print(resultado)
+midb.commit() #compromete datos para que se ejecute la consulte
